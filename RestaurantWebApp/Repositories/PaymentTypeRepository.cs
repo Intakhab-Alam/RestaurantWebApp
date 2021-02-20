6cd14ee50d83c07accde0baa7880a40c4660f166
Intakhab Alam <intakhabnith@gmail.com>
2021-02-20 18:04:02 +0530

    Add project files.

---
+++ RestaurantWebApp/Repositories/PaymentTypeRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RestaurantWebApp.Models;
+
+namespace RestaurantWebApp.Repositories
+{
+    public class PaymentTypeRepository
+    {
+        private RestaurantDBEntities objRestaurantDBEntities;
+        public PaymentTypeRepository()
+        {
+            objRestaurantDBEntities = new RestaurantDBEntities();
+        }
+        public IEnumerable<SelectListItem> GetAllPaymentType()
+        {
+            var objSelectListItems = new List<SelectListItem>();
+            objSelectListItems = (from obj in objRestaurantDBEntities.PaymentTypes
+                                  select new SelectListItem()
+                                  {
+                                      Text = obj.PaymentTypeName,
+                                      Value = obj.PaymentTypeId.ToString(),
+                                      Selected = true
+                                  }).ToList();
+
+            return objSelectListItems;
+        }
+    }
+}
